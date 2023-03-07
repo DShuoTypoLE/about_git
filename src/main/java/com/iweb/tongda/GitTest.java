cb3ac9c479820953bf50ae023dd55b2532964a19
DingSh <DingSh@personal.com>
2023-03-07 12:49:50 +0800

    first commit

---
+++ src/main/java/com/iweb/tongda/GitTest.java
@@ -0,0 +1,12 @@
+package com.iweb.tongda;
+
+/**
+ * @author 丁硕
+ * @version 1.0
+ * @Date 2023-03-07 12:38
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello,git!!!");
+    }
+}
